8ba7aab7daf21e9124ae0165d6bb97a736406ad2
Mike Oz <kozloffsky@gmail.com>
2014-09-28 17:51:09 +0300

    server resolcer path

---
+++ src/Oz/WhiteHowk/Module/Page/DependencyInjection/ServiceResolvercompilerPass.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 9/25/14
+ * Time: 18:04
+ */
+
+namespace Oz\WhiteHowk\Module\Page\DependencyInjection;
+
+
+class ServiceResolvercompilerPass {
+
+} 
