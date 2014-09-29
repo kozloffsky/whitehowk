6bbd891baada0b7bafec50d4d296d86741b0c19b
Mike Oz <kozloffsky@gmail.com>
2014-09-29 22:25:57 +0300

    remote services and hash client router service

---
+++ src/Oz/WhiteHowk/Module/Page/DependencyInjection/ServiceResolvercompilerPass.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 9/25/14
+ * Time: 18:04
+ */
+
+namespace Oz\WhiteHowk\Module\Page\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ServiceResolverCompilerPass implements CompilerPassInterface{
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     *
+     * @api
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $serviceResolverId = 'page.service_resolver';
+        $resolverDefinition = $container->getDefinition($serviceResolverId);
+        foreach($container->findTaggedServiceIds('page.remote_service') as $id=>$params){
+            foreach($params as $param){
+                $methods = explode(',',$param['methods']);
+                $resolverDefinition->addMethodCall('addService', array($id, $container->getDefinition($id), $methods));
+            }
+        }
+    }
+
+} 
